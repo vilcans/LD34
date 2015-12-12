6b854affe44dbcdb9ece3f8d5f15a36121fe2672
Martin Vilcans <martin@librador.com>
2015-12-13 00:06:32 +0100

    Add pickup cones

---
+++ Assets/PickingUp.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class PickingUp : MonoBehaviour {
+
+    public void OnTriggerEnter(Collider collider) {
+        //Debug.LogFormat("Collided with {0}", collider);
+        Destroy(collider.gameObject);
+    }
+}
