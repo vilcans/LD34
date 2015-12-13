9fa5b2b46f0df10f256dd8e066f4ad70170ac985
Martin Vilcans <martin@librador.com>
2015-12-13 15:52:41 +0100

    Restart scene after game over

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -6,16 +6,29 @@ public class GameController : MonoBehaviour {
     public GameObject gameOverUI;
     public Text endText;
 
+    public float restartDelay = 2.0f;
+
     private enum State {
         Playing,
         GameOver,
     }
     private State state;
 
+    private float gameOverProgress;
+
     void Start() {
         gameOverUI.SetActive(false);
         endText.enabled = false;
         state = State.Playing;
+    }
+
+    void Update() {
+        if(state == State.GameOver) {
+            gameOverProgress += Time.deltaTime;
+            if(gameOverProgress >= restartDelay) {
+                Application.LoadLevel(0);
+            }
+        }
     }
 
     public void Kill() {
@@ -31,6 +44,8 @@ public class GameController : MonoBehaviour {
     }
 
     private void StopGame(string reason) {
+        state = State.GameOver;
+        gameOverProgress = 0;
         endText.text = reason;
         endText.enabled = true;
         gameOverUI.SetActive(true);
