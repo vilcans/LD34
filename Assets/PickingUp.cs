016b9e3fc5baf7f110ccf64dbb167e85d7d1ffe1
Martin Vilcans <martin@librador.com>
2015-12-13 09:13:34 +0100

    Add pickup sound

--- Assets/PickingUp.cs
+++ Assets/PickingUp.cs
@@ -2,8 +2,12 @@
 
 public class PickingUp : MonoBehaviour {
 
+    public AudioClip pickupAudio;
+    public float audioVolume = 1;
+
     public void OnTriggerEnter(Collider collider) {
         //Debug.LogFormat("Collided with {0}", collider);
         Destroy(collider.gameObject);
+        AudioSource.PlayClipAtPoint(pickupAudio, collider.transform.position, audioVolume);
     }
 }
